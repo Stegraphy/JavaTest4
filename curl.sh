d12bc5a6556e42c43004562fb2094e0f77b8e90c
CONG.LEI01 <507452965@qq.com>
2019-07-26 12:52:10 +0800

    curl and config

--- curl.sh
+++ curl.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 #JAVA_IP=localhost
-JAVA_IP=192.168.99.100
+JAVA_IP=192.168.174.129
 
 echo "=========>login"
-curl -H "Content-Type:application/json" -X POST --data '{"name": "MARY","password":"SMITH"}' http://$JAVA_IP:9095/login -s
+curl -H "Content-Type:application/json" -X POST --data '{"name": "MARY","password":"SMITH"}' http://$JAVA_IP:18080/login -s
 
 echo "=========> use pagehelper select"
-curl -H "Content-Type:application/json" -X GET --data '{"page": 3,"pageSize":4}' http://$JAVA_IP:9095/film/list
+curl -H "Content-Type:application/json" -X GET --data '{"page": 3,"pageSize":4}' http://$JAVA_IP:18080/film/list
 
 echo "===============> insert customer"
 #first_name, last_name, email, address
-curl -H "Content-Type:application/json" -X PUT --data '{"first_name": "leo","last_name":"li","email":"test@qq.com","address":"47 MySakila Drive"}' http://$JAVA_IP:9095/customer
+curl -H "Content-Type:application/json" -X PUT --data '{"first_name": "leo","last_name":"li","email":"test@qq.com","address":"47 MySakila Drive"}' http://$JAVA_IP:18080/customer
 
 echo "================>update customer"
-curl -H "Content-Type:application/json" -X POST --data '{"first_name": "leoupdate","last_name":"li","email":"test@qq.com","address":"47 MySakila Drive"}' http://$JAVA_IP:9095/customer/600
+curl -H "Content-Type:application/json" -X POST --data '{"first_name": "leoupdate","last_name":"li","email":"test@qq.com","address":"47 MySakila Drive"}' http://$JAVA_IP:18080/customer/600
 
 echo "============> delete customer"
-curl -X DELETE --cookie "id=600" http://$JAVA_IP:9095/customer/600
+curl -X DELETE --cookie "id=600" http://$JAVA_IP:18080/customer/600
