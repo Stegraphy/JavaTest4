1965dd209106dd8d1ea07b100bbb7be1f2f570e6
CONG.LEI01 <507452965@qq.com>
2019-07-26 16:46:18 +0800

    Commit HandlerMethodArgumentResolve method

--- curl.sh
+++ curl.sh
@@ -12,10 +12,10 @@ curl -H "Content-Type:application/json" -X GET --data '{"page": 3,"pageSize":4}'
 
 echo "===============> insert customer"
 #first_name, last_name, email, address
-curl -H "Content-Type:application/json" -X PUT --data '{"first_name": "leo","last_name":"li","email":"test@qq.com","address":"47 MySakila Drive"}' http://$JAVA_IP:18080/customer
+curl -H "Content-Type:application/json" -X POST --data '{"first_name": "lei","last_name":"cong","email":"testgmail@qq.com","address":"47 MySakila Drive"}' http://$JAVA_IP:18080/customer
 
 echo "================>update customer"
-curl -H "Content-Type:application/json" -X POST --data '{"first_name": "leoupdate","last_name":"li","email":"test@qq.com","address":"47 MySakila Drive"}' http://$JAVA_IP:18080/customer/600
+curl -H "Content-Type:application/json" -X PUT --data '{"first_name": "leiupdate","last_name":"cong","email":"testgmail@qq.com","address":"47 MySakila Drive"}' http://$JAVA_IP:18080/customer/600
 
 echo "============> delete customer"
 curl -X DELETE --cookie "id=600" http://$JAVA_IP:18080/customer/600
